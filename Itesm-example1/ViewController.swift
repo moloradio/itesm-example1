494aef33cf6ab5bca19d108f56c1ad36f211d91d
moloradio <aldo@macbook-pro-de-aldo.local>
2016-06-06 02:17:34 -0500

    Commit 1

--- Itesm-example1/ViewController.swift
+++ Itesm-example1/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var libro: UITextField!
+    @IBOutlet weak var respuesta: UITextView!
+    
+    @IBAction func buscar(sender: AnyObject) {
+        asincrono()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var limpiar: UIButton!
+    
+    @IBAction func limpiar(sender: AnyObject) {
+        libro.text = ""
+    }
+    
+    
+    func asincrono(){
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + libro.text!
+        let url = NSURL(string: urls)
+        let sesion = NSURLSession.sharedSession()
+        let bloque = {(datos: NSData?, resp:NSURLResponse?, error: NSError?) -> Void in
+            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            print(texto!)
+            //self.respuesta.text = texto
+        }
+        
+        let dt = sesion.dataTaskWithURL(url!, completionHandler: bloque)
+        dt.resume()
+    }
+    
+    func sincrono(){
+        let urls = "http://dia.ccm.itesm.mx/"
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        let texto = NSString(data:datos!, encoding: NSUTF8StringEncoding)
+        print(texto!)
     }
 
     override func didReceiveMemoryWarning() {
